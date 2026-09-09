077bf76c0243b5ee0d2fcaa99d66e7f6246fdd44
Sohan Maharjan <sohanmaharjan444@gmail.com>
2026-09-09 08:52:38 +0545

    react vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className='bg-blue-500'>tailwind is working</p>
+    <One/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/one.tsx
@@ -0,0 +1,9 @@
+function One(){
+    return
+    {
+        <>
+        <p className="bg-red-50">this is one</p>
+        </>
+    }
+}
+export default One
