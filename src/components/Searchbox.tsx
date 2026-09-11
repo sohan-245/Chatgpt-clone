73d5fdcdf0bb3dfae03a7a83f807de574109de45
Sohan Maharjan <sohanmaharjan444@gmail.com>
2026-09-11 12:39:17 +0545

    searchbar fixed

--- src/components/Searchbox.tsx
+++ src/components/Searchbox.tsx
@@ -3,7 +3,7 @@ function SearchBox(){
         <>
         <div className="flex flex-row space-between border-2 m-6 p-1 items-center rounded-4xl w-200 border-gray-300">
         <img src="./plus.png"/>
-        <div className=" w-170"><input type="text" placeholder="Ask ChatGpt" /></div>
+        <div><input type="text" placeholder="Ask ChatGpt" className="w-170 outline-none"  /></div>
         <div className="flex flex-row gap-3 m-1 items-center">
         <img src="./mic.png" alt="" style={{height:"20px"}} />
         <img src="./voice.png" alt="" style={{height:"30px", width:"30px"}} />
